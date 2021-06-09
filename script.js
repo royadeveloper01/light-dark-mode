fef8eaae424c76b02acf9d98c762efdc26e92b87
Chimaobi Prince <royadeveloper01@gmail.com>
2021-06-09 04:34:08 +0100

    update

--- script.js
+++ script.js
@@ -8,25 +8,41 @@ const textBox = document.getElementById('text-box');
 
 // Dark Mode Styles
 function darkMode() {
-    nav.style.backgroundColor = 'teal';
-    textBox.style.backgroundColor = 'white';
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 60%)'
     toggleIcon.children[0].classList.replace('fa-sun', 'fa-moon');
 }
 
 // Light Mode Styles
 function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 80%)';
     toggleIcon.children[0].classList.replace('fa-moon', 'fa-sun');
 }
 // Switch Theme Dynamically
 function switchTheme(e) {
     if(e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
-// Event Listener
+// Event Listener 
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Get the current theme from Local Storage
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
